6284f955025ad0f7990f0a386df7ebde19ca90db
Carrie Savinell <carrielane89@gmail.com>
2018-07-03 13:16:32 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,10 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
-  sql = <<-sql
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      
-    )
-  SQL
+  def change
+    
+  end
 end
